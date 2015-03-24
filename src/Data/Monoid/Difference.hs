311fc9eb0d8a485f7b12ee51b5d67f9c9cdfa7c1
Adrian Keet <arkeet@gmail.com>
2015-03-24 20:21:49 +0000

    Inline all the things, re-export Endo, and add Typeable/Generic instances

--- src/Data/Monoid/Difference.hs
+++ src/Data/Monoid/Difference.hs
@@ -1,5 +1,7 @@
 module Data.Monoid.Difference (
 
+    Endo(..),
+
 -- ** Difference monoid
     improve,
     unimprove,
@@ -11,7 +13,9 @@ import Data.Monoid
 -- | Converting a normal monoid to a difference monoid.
 improve :: Monoid a => a -> Endo a
 improve a = Endo (mappend a)
+{-# INLINE improve #-}
 
 -- | Converting a difference monoid back to a normal monoid.
 unimprove :: Monoid a => Endo a -> a
 unimprove (Endo f) = f mempty
+{-# INLINE unimprove #-}
--- src/Data/Semigroup/Difference.hs
+++ src/Data/Semigroup/Difference.hs
@@ -1,3 +1,15 @@
+{-# LANGUAGE CPP #-}
+
+#if defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ >= 704
+#define LANGUAGE_DeriveGeneric
+{-# LANGUAGE DeriveGeneric #-}
+#endif
+
+#ifdef __GLASGOW_HASKELL__
+#define LANGUAGE_DeriveDataTypeable
+{-# LANGUAGE DeriveDataTypeable #-}
+#endif
+
 module Data.Semigroup.Difference (
 
 -- ** Difference semigroup
@@ -16,35 +28,60 @@ import Data.List.NonEmpty
 import Data.Semigroup
 import Data.Semigroup.Foldable
 
+#ifdef LANGUAGE_DeriveDataTypeable
+import Data.Typeable
+#endif
+
+#ifdef LANGUAGE_DeriveGeneric
+import GHC.Generics
+#endif
+
 import Prelude hiding (foldr1, foldl1)
 
 
 -- | Difference semigroup.
 data Diff a = Diff (a -> a) a
+    deriving (
+#ifdef LANGUAGE_DeriveDataTypeable
+    Typeable
+#ifdef LANGUAGE_DeriveGeneric
+    ,
+#endif
+#endif
+#ifdef LANGUAGE_DeriveGeneric
+    Generic
+#endif
+    )
 
 instance Semigroup (Diff a) where
     ~(Diff a' _) <> ~(Diff b' b) = Diff (a' . b') (a' b)
 
 instance Monoid a => Monoid (Diff a) where
     mempty = Diff id mempty
+    {-# INLINE mempty #-}
     mappend = (<>)
+    {-# INLINE mappend #-}
 
 -- | Converting a normal semigroup to a difference semigroup.
 improve :: Semigroup a => a -> Diff a
 improve a = Diff (a <>) a
+{-# INLINE improve #-}
 
 -- | Converting a difference semigroup back to a normal semigroup.
 unimprove :: Diff a -> a
 unimprove ~(Diff _ a) = a
+{-# INLINE unimprove #-}
 
 
 -- | One direction of the isomorphism between @'Diff' a@ and @'Maybe' a -> a@.
 maybeDiff :: Diff a -> Maybe a -> a
 maybeDiff (Diff a' a) = maybe a a'
+{-# INLINE maybeDiff #-}
 
 -- | The other direction of the isomorphism between @'Diff' a@ and @'Maybe' a -> a@.
 diffMaybe :: (Maybe a -> a) -> Diff a
 diffMaybe f = Diff (f . Just) (f Nothing)
+{-# INLINE diffMaybe #-}
 
 
 -- These folds should really be in Data.Semigroup.Foldable.
@@ -52,19 +89,24 @@ diffMaybe f = Diff (f . Just) (f Nothing)
 -- | Right-associative fold of a nonempty structure, with a base case.
 foldr1m :: Foldable1 t => (a -> Maybe b -> b) -> t a -> b
 foldr1m f = unimprove . foldMap1 (diffMaybe . f)
+{-# INLINE foldr1m #-}
 
 -- | Right-associative fold of a nonempty structure.
 foldr1 :: Foldable1 t => (a -> a -> a) -> t a -> a
 foldr1 f = unimprove . foldMap1 (\a -> Diff (f a) a)
+{-# INLINE foldr1 #-}
 
 -- | Left-associative fold of a nonempty structure, with a base case.
 foldl1m :: Foldable1 t => (Maybe b -> a -> b) -> t a -> b
 foldl1m f = unimprove . getDual . foldMap1 (Dual . diffMaybe . flip f)
+{-# INLINE foldl1m #-}
 
 -- | Left-associative fold of a nonempty structure.
 foldl1 :: Foldable1 t => (a -> a -> a) -> t a -> a
 foldl1 f = unimprove . getDual . foldMap1 (\a -> Dual $ Diff (flip f a) a)
+{-# INLINE foldl1 #-}
 
 -- | Nonempty list of elements of a nonempty structure.
 toList1 :: Foldable1 t => t a -> NonEmpty a
 toList1 = foldr1m (\a -> maybe (a :| []) (a <|))
+{-# INLINE toList1 #-}
